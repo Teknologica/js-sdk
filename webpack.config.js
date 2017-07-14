679385cdbc2d22184ca59118db76aaf0673391d5
Olivier <olivier.tr@hotmail.com>
2017-07-14 16:37:14 -0400

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,37 +5,38 @@ const rules = [
     {test: /\.js$/, use: 'babel-loader'}
 ];
 
-var nodeConfig = {
-    entry: './src/index.js',
-    target: 'node',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: process.env.NODE_ENV === 'production'
-        })
-    ]
+module.exports = (env = {}) => {
+    console.log('env', env);
+    return [{
+        entry: './src/index.js',
+        target: 'node',
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules
+        },
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: false
+            })
+        ],
+        devtool: env.production ? 'source-map' : 'cheap-eval-source-map'
+    }, {
+        entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, './dist'),
+            filename: 'bundle.node.js'
+        },
+        module: {
+            rules
+        },
+        plugins: [
+            new webpack.optimize.UglifyJsPlugin({
+                compress: false
+            })
+        ],
+        devtool: env.production ? 'source-map' : 'cheap-eval-source-map'
+    }]
 };
-
-var browserConfig = {
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: 'bundle.node.js'
-    },
-    module: {
-        rules
-    },
-    plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: process.env.NODE_ENV === 'production'
-        })
-    ]
-};
-
-module.exports = (env = {}) => [nodeConfig, browserConfig];
