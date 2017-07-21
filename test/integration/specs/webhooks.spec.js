eb3809a7a9e603ddc830d44fde9497d64be500b8
Andrei Moldoveanu <andrei@ilabsinc.com>
2017-07-21 10:44:42 -0400

    fix tabulation, cleanup integration tests

--- test/integration/specs/webhooks.spec.js
+++ test/integration/specs/webhooks.spec.js
@@ -5,57 +5,46 @@ import {createWebhookCredData, createWebhookData} from '../utils';
 const expect = chai.expect;
 
 describe('when using the webhooks resource', () => {
-  const testIds = {with: null, without: null};
-  let sharedData = {};
+    const testIds = {with: null, without: null};
+    let sharedData = {};
 
-  before(async () => {
-    const data = createWebhookCredData();
-    try {
-      const WebhookCredHash = await apiInstance.credentialHashes.createWebhookCredential({data:data});
-      sharedData.credentialHash = WebhookCredHash.fields.hash;
-    }
-    catch(err) {
-      console.log(err);
-    }
+    before(async () => {
+        const data = createWebhookCredData();
+        const WebhookCredHash = await apiInstance.credentialHashes.createWebhookCredential({data: data});
+        sharedData.credentialHash = WebhookCredHash.fields.hash;
+    });
 
-  });
+    it('I can create a webhook without an ID', async () => {
+        const data = createWebhookData(false, sharedData);
+        const webhook = await apiInstance.webhooks.create({data: data});
+        testIds.without = webhook.fields.id;
+        expect(webhook.fields.value).to.be.equal(data.value);
+    });
 
+    it('I can create a webhook with an ID', async () => {
+        const {id, ...data} = createWebhookData(true, sharedData);
+        const webhook = await apiInstance.webhooks.create({id, data});
+        expect(webhook.fields.id).to.be.equal(id);
+        testIds.with = id;
+        expect(webhook.fields.value).to.be.equal(data.value);
+    });
 
-  it('I can create a webhook item without an ID', async () => {
-    const data = createWebhookData(false, sharedData);
-    const webhook = await apiInstance.webhooks.create({data: data});
-    testIds.without = webhook.fields.id;
-    expect(webhook.fields.value).to.be.equal(data.value);
-  });
+    it('I can get a list of webhooks', async () => {
+        const webhook = await apiInstance.webhooks.getAll();
+        expect(webhook.total).to.not.be.equal(0);
+        const [webhookItems] = webhook.items;
+        expect(webhookItems.fields.id).to.not.be.undefined;
+    });
 
-  it('I can create a webhook item with an ID', async () => {
-    const {id, ...data} = createWebhookData(true, sharedData);
-    const webhook = await apiInstance.webhooks.create({id, data});
-    expect(webhook.fields.id).to.be.equal(id);
-    testIds.with = id;
-    expect(webhook.fields.value).to.be.equal(data.value);
-  });
+    it('I can get a webhook by using its ID', async () => {
+        const webhookItem = await apiInstance.webhooks.get({id: testIds.with});
+        expect(webhookItem.fields.id).to.be.equal(testIds.with);
+    });
 
-  it('I can get a list of webhook items', async () => {
-    const webhook = await apiInstance.webhooks.getAll();
-    expect(webhook.total).to.not.be.equal(0);
-    const [webhookItems] = webhook.items;
-    expect(webhookItems.fields.id).to.not.be.undefined;
-  });
-
-  it('I can get a webhook item by using its ID', async () => {
-    const webhookItem = await apiInstance.webhooks.get({id: testIds.with});
-    expect(webhookItem.fields.id).to.be.equal(testIds.with);
-  });
-
-  it('I can update a webhook by using its ID', async () => {
-    const data = createWebhookData(false, sharedData);
-    const webhook = await apiInstance.webhooks.update({id: testIds.with, data: data});
-    expect(webhook.fields.value).to.be.equal(data.value);
-    expect(webhook.response.status).to.be.equal(200);
-
-  });
-
+    it('I can update a webhook by using its ID', async () => {
+        const data = createWebhookData(false, sharedData);
+        const webhook = await apiInstance.webhooks.update({id: testIds.with, data: data});
+        expect(webhook.fields.value).to.be.equal(data.value);
+        expect(webhook.response.status).to.be.equal(200);
+    });
 });
-
-
--- test/integration/specs/websites.spec.js
+++ test/integration/specs/websites.spec.js
@@ -5,52 +5,52 @@ import {createWebsiteData} from '../utils';
 const expect = chai.expect;
 
 describe('when using the websites resource', () => {
-  const testIds = {with: null, without: null};
+    const testIds = {with: null, without: null};
 
-  it('I can create a website without an ID', async () => {
-    const data = createWebsiteData();
-    const website = await apiInstance.websites.create({data: data});
-    testIds.without = website.fields.id;
-    expect(website.fields.value).to.be.equal(data.value);
-  });
+    it('I can create a website without an ID', async () => {
+        const data = createWebsiteData();
+        const website = await apiInstance.websites.create({data: data});
+        testIds.without = website.fields.id;
+        expect(website.fields.value).to.be.equal(data.value);
+    });
 
-  it('I can create a website with an ID', async () => {
-    const {id, ...data} = createWebsiteData(true);
-    const website = await apiInstance.websites.create({id, data});
-    expect(website.fields.id).to.be.equal(id);
-    testIds.with = id;
-    expect(website.fields.value).to.be.equal(data.value);
-  });
-
-
-  it('I can get a list of websites', async () => {
-    const websites = await apiInstance.websites.getAll();
-    expect(websites.total).to.not.be.equal(0);
-    const [websitesItem] = websites.items;
-    expect(websitesItem.fields.id).to.not.be.undefined;
-  });
+    it('I can create a website with an ID', async () => {
+        const {id, ...data} = createWebsiteData(true);
+        const website = await apiInstance.websites.create({id, data});
+        expect(website.fields.id).to.be.equal(id);
+        testIds.with = id;
+        expect(website.fields.value).to.be.equal(data.value);
+    });
 
 
-  it('I can get a website by using its ID', async () => {
-    const websiteItem = await apiInstance.websites.get({id: testIds.with});
-    expect(websiteItem.fields.id).to.be.equal(testIds.with);
-  });
-
-  it('I can update a website by using its ID', async () => {
-    const data = createWebsiteData();
-    const website = await apiInstance.websites.update({id: testIds.with, data: data});
-    expect(website.fields.value).to.be.equal(data.value);
-    expect(website.response.status).to.be.equal(200);
-
-  });
+    it('I can get a list of websites', async () => {
+        const websites = await apiInstance.websites.getAll();
+        expect(websites.total).to.not.be.equal(0);
+        const [websitesItem] = websites.items;
+        expect(websitesItem.fields.id).to.not.be.undefined;
+    });
 
 
-  it('I can delete the websites I just created', async () => {
-    const firstDelete = await apiInstance.websites.delete({id: testIds.with});
-    const secondDelete = await apiInstance.websites.delete({id: testIds.without});
-    expect(firstDelete.response.status).to.be.equal(204);
-    expect(secondDelete.response.status).to.be.equal(204);
-  });
+    it('I can get a website by using its ID', async () => {
+        const websiteItem = await apiInstance.websites.get({id: testIds.with});
+        expect(websiteItem.fields.id).to.be.equal(testIds.with);
+    });
+
+    it('I can update a website by using its ID', async () => {
+        const data = createWebsiteData();
+        const website = await apiInstance.websites.update({id: testIds.with, data: data});
+        expect(website.fields.value).to.be.equal(data.value);
+        expect(website.response.status).to.be.equal(200);
+
+    });
+
+
+    it('I can delete the websites I just created', async () => {
+        const firstDelete = await apiInstance.websites.delete({id: testIds.with});
+        const secondDelete = await apiInstance.websites.delete({id: testIds.without});
+        expect(firstDelete.response.status).to.be.equal(204);
+        expect(secondDelete.response.status).to.be.equal(204);
+    });
 
 
 });
