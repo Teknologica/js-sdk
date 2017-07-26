02d0c2e7c05773b3cdd0f207a143578651beded7
Olivier <olivier.tr@hotmail.com>
2017-07-26 12:51:54 -0400

    remove console statement in profile

--- test/integration/specs/profile.spec.js
+++ test/integration/specs/profile.spec.js
@@ -5,16 +5,13 @@ import {createProfileData} from '../utils.js';
 const expect = chai.expect;
 
 describe('when using the profile resource', () => {
-
     let sharedProfileId;
 
     it('I can get a profile', async() => {
         const profile = await apiInstance.profile.get();
         expect(profile.response.status).to.be.equal(200);
-        console.log(profile);
         sharedProfileId = profile.fields.id;
     });
-
 
     it('I can update a profile', async() => {
         const data = createProfileData();
@@ -22,11 +19,9 @@ describe('when using the profile resource', () => {
         expect(profile.response.status).to.be.equal(200);
     });
 
-
     it.skip('I can update the password of a profile', async() => {
 
     });
-
 
     it('I can reset totp', async () => {
         const totpSecret = await apiInstance.profile.resetTotp();
