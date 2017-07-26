fb004ce1c37e705bd4b87d71597671fea5eae861
gikgit <hejin.udem@gmail.com>
2017-07-26 17:14:43 -0400

    histograms spec fail

--- test/integration/specs/experimental/histograms.spec.js
+++ test/integration/specs/experimental/histograms.spec.js
@@ -7,10 +7,10 @@ describe('when using the histograms experimental resource', () => {
 
     it('I can get transaction histogram report data', async () => {
         const data = await experimentalApiInstance.histograms.getTransactionHistogramReport({periodStart: '2017-07-19T00:00:00Z',
-                                                                                             periodEnd: '2017-07-28T23:59:59Z',
-                                                                                            aggregationField: 'transactionResult',
+                                                                                             periodEnd: '2017-07-23T23:59:59Z',
+                                                                                            aggregationField: 'website',
                                                                                             aggregationPeriod: 'day',
-                                                                                            metric: 'ApprovalThroughput' });
+                                                                                            metric: 'sales' });
         console.log(data);
     });
 });
