939862b42c1bf8d1ed0fe630ebcbdbded8b30687
Olivier <olivier.tr@hotmail.com>
2017-07-26 14:57:38 -0400

    small fix

--- test/integration/specs/experimental/reports.spec.js
+++ test/integration/specs/experimental/reports.spec.js
@@ -5,7 +5,7 @@ const expect = chai.expect;
 
 describe('when using the cumulative subscriptions plans experimental resource', () => {
     it('I can retrieve a cumulative subscriptions plans report', async () => {
-        const report = await experimentalApiInstance.cumulativeSubscriptionsPlans.getAll({periodStart: '2017-07-19T00:00:00Z', periodEnd: '2017-07-26T23:59:59Z'});
+        const report = await experimentalApiInstance.reports.getAll({periodStart: '2017-07-19T00:00:00Z', periodEnd: '2017-07-26T23:59:59Z'});
         console.log(report.items[0].fields);
     });
 });
